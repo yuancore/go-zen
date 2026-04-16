97c43386dc595861c2e5c2f580aab8c2233cdfc4
xianglong <56494565@qq.com>
2026-04-17 05:21:27 +0800

    no message

--- adapter/db/zdb/request_test.go
+++ adapter/db/zdb/request_test.go
@@ -2,10 +2,13 @@ package zdb
 
 import (
 	"context"
+	"io"
+	"io/fs"
 	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/yuancore/go-zen/zen"
@@ -120,92 +123,150 @@ func newMockGormDB(t *testing.T) *gorm.DB {
 
 type requestContextKey string
 
+// testZenContext is a minimal zen.Context stub used only in tests.
+// Only Request() is meaningful; all other methods return zero values.
 type testZenContext struct {
 	request *http.Request
 }
 
-// --- Path / Query ---
-func (c testZenContext) Param(string) string                { return "" }
-func (c testZenContext) Query(string) string                { return "" }
-func (c testZenContext) DefaultQuery(string, string) string { return "" }
-func (c testZenContext) GetQuery(string) (string, bool)     { return "", false }
-func (c testZenContext) QueryArray(string) []string         { return nil }
-func (c testZenContext) QueryMap(string) map[string]string  { return nil }
+// --- context.Context ---
+func (c testZenContext) Deadline() (time.Time, bool) { return time.Time{}, false }
+func (c testZenContext) Done() <-chan struct{}       { return nil }
+func (c testZenContext) Err() error                  { return nil }
+func (c testZenContext) Value(any) any               { return nil }
 
-// --- Headers ---
-func (c testZenContext) Header(string) string        { return "" }
-func (c testZenContext) SetHeader(string, string)    {}
-func (c testZenContext) ContentType() string         { return "" }
-func (c testZenContext) GetRawData() ([]byte, error) { return nil, nil }
-func (c testZenContext) IsWebsocket() bool           { return false }
+// --- URL params ---
+func (c testZenContext) Param(string) string                          { return "" }
+func (c testZenContext) AddParam(string, string)                      {}
+func (c testZenContext) Query(string) string                          { return "" }
+func (c testZenContext) DefaultQuery(string, string) string           { return "" }
+func (c testZenContext) GetQuery(string) (string, bool)               { return "", false }
+func (c testZenContext) QueryArray(string) []string                   { return nil }
+func (c testZenContext) GetQueryArray(string) ([]string, bool)        { return nil, false }
+func (c testZenContext) QueryMap(string) map[string]string            { return nil }
+func (c testZenContext) GetQueryMap(string) (map[string]string, bool) { return nil, false }
+
+// --- Request info ---
+func (c testZenContext) Header(string) string                { return "" }
+func (c testZenContext) SetHeader(string, string)            {}
+func (c testZenContext) ContentType() string                 { return "" }
+func (c testZenContext) GetRawData() ([]byte, error)         { return nil, nil }
+func (c testZenContext) IsWebsocket() bool                   { return false }
+func (c testZenContext) RemoteIP() string                    { return "" }
+func (c testZenContext) ClientIP() string                    { return "" }
+func (c testZenContext) FullPath() string                    { return "" }
+func (c testZenContext) Request() *http.Request              { return c.request }
+func (c testZenContext) ResponseWriter() http.ResponseWriter { return httptest.NewRecorder() }
+func (c testZenContext) HandlerName() string                 { return "" }
+func (c testZenContext) HandlerNames() []string              { return nil }
 
 // --- Bind ---
 func (c testZenContext) Bind(any) error       { return nil }
 func (c testZenContext) BindJSON(any) error   { return nil }
+func (c testZenContext) BindXML(any) error    { return nil }
 func (c testZenContext) BindQuery(any) error  { return nil }
+func (c testZenContext) BindYAML(any) error   { return nil }
+func (c testZenContext) BindTOML(any) error   { return nil }
+func (c testZenContext) BindPlain(any) error  { return nil }
 func (c testZenContext) BindUri(any) error    { return nil }
 func (c testZenContext) BindHeader(any) error { return nil }
 
 // --- ShouldBind ---
-func (c testZenContext) ShouldBind(any) error       { return nil }
-func (c testZenContext) ShouldBindJSON(any) error   { return nil }
-func (c testZenContext) ShouldBindQuery(any) error  { return nil }
-func (c testZenContext) ShouldBindUri(any) error    { return nil }
-func (c testZenContext) ShouldBindHeader(any) error { return nil }
+func (c testZenContext) ShouldBind(any) error              { return nil }
+func (c testZenContext) ShouldBindJSON(any) error          { return nil }
+func (c testZenContext) ShouldBindXML(any) error           { return nil }
+func (c testZenContext) ShouldBindQuery(any) error         { return nil }
+func (c testZenContext) ShouldBindYAML(any) error          { return nil }
+func (c testZenContext) ShouldBindTOML(any) error          { return nil }
+func (c testZenContext) ShouldBindPlain(any) error         { return nil }
+func (c testZenContext) ShouldBindUri(any) error           { return nil }
+func (c testZenContext) ShouldBindHeader(any) error        { return nil }
+func (c testZenContext) ShouldBindBodyWithJSON(any) error  { return nil }
+func (c testZenContext) ShouldBindBodyWithXML(any) error   { return nil }
+func (c testZenContext) ShouldBindBodyWithYAML(any) error  { return nil }
+func (c testZenContext) ShouldBindBodyWithTOML(any) error  { return nil }
+func (c testZenContext) ShouldBindBodyWithPlain(any) error { return nil }
 
 // --- Form ---
-func (c testZenContext) PostForm(string) string                { return "" }
-func (c testZenContext) DefaultPostForm(string, string) string { return "" }
-func (c testZenContext) GetPostForm(string) (string, bool)     { return "", false }
-func (c testZenContext) PostFormArray(string) []string         { return nil }
-func (c testZenContext) PostFormMap(string) map[string]string  { return nil }
+func (c testZenContext) PostForm(string) string                          { return "" }
+func (c testZenContext) DefaultPostForm(string, string) string           { return "" }
+func (c testZenContext) GetPostForm(string) (string, bool)               { return "", false }
+func (c testZenContext) PostFormArray(string) []string                   { return nil }
+func (c testZenContext) GetPostFormArray(string) ([]string, bool)        { return nil, false }
+func (c testZenContext) PostFormMap(string) map[string]string            { return nil }
+func (c testZenContext) GetPostFormMap(string) (map[string]string, bool) { return nil, false }
 
 // --- Multipart ---
-func (c testZenContext) FormFile(string) (*multipart.FileHeader, error)       { return nil, nil }
-func (c testZenContext) MultipartForm() (*multipart.Form, error)              { return nil, nil }
-func (c testZenContext) SaveUploadedFile(*multipart.FileHeader, string) error { return nil }
+func (c testZenContext) FormFile(string) (*multipart.FileHeader, error) { return nil, nil }
+func (c testZenContext) MultipartForm() (*multipart.Form, error)        { return nil, nil }
+func (c testZenContext) SaveUploadedFile(*multipart.FileHeader, string, ...fs.FileMode) error {
+	return nil
+}
 
 // --- Cookie ---
 func (c testZenContext) Cookie(string) (string, error)                             { return "", nil }
 func (c testZenContext) SetCookie(string, string, int, string, string, bool, bool) {}
+func (c testZenContext) SetCookieData(*http.Cookie)                                {}
 func (c testZenContext) SetSameSite(http.SameSite)                                 {}
 
 // --- Response ---
-func (c testZenContext) JSON(int, any)              {}
-func (c testZenContext) SecureJSON(int, any)        {}
-func (c testZenContext) JSONP(int, any)             {}
-func (c testZenContext) IndentedJSON(int, any)      {}
-func (c testZenContext) AsciiJSON(int, any)         {}
-func (c testZenContext) PureJSON(int, any)          {}
-func (c testZenContext) XML(int, any)               {}
-func (c testZenContext) YAML(int, any)              {}
-func (c testZenContext) String(int, string, ...any) {}
-func (c testZenContext) Data(int, string, []byte)   {}
-func (c testZenContext) Status(int)                 {}
-func (c testZenContext) Redirect(int, string)       {}
+func (c testZenContext) JSON(int, any)                                                   {}
+func (c testZenContext) SecureJSON(int, any)                                             {}
+func (c testZenContext) JSONP(int, any)                                                  {}
+func (c testZenContext) IndentedJSON(int, any)                                           {}
+func (c testZenContext) AsciiJSON(int, any)                                              {}
+func (c testZenContext) PureJSON(int, any)                                               {}
+func (c testZenContext) HTML(int, string, any)                                           {}
+func (c testZenContext) XML(int, any)                                                    {}
+func (c testZenContext) YAML(int, any)                                                   {}
+func (c testZenContext) TOML(int, any)                                                   {}
+func (c testZenContext) ProtoBuf(int, any)                                               {}
+func (c testZenContext) BSON(int, any)                                                   {}
+func (c testZenContext) String(int, string, ...any)                                      {}
+func (c testZenContext) Data(int, string, []byte)                                        {}
+func (c testZenContext) DataFromReader(int, int64, string, io.Reader, map[string]string) {}
+func (c testZenContext) Status(int)                                                      {}
+func (c testZenContext) Redirect(int, string)                                            {}
+func (c testZenContext) File(string)                                                     {}
+func (c testZenContext) FileFromFS(string, http.FileSystem)                              {}
+func (c testZenContext) FileAttachment(string, string)                                   {}
+func (c testZenContext) SSEvent(string, any)                                             {}
+func (c testZenContext) Stream(func(io.Writer) bool) bool                                { return false }
+func (c testZenContext) NegotiateFormat(...string) string                                { return "" }
+func (c testZenContext) SetAccepted(...string)                                           {}
 
-// --- Underlying ---
-func (c testZenContext) Request() *http.Request              { return c.request }
-func (c testZenContext) ResponseWriter() http.ResponseWriter { return httptest.NewRecorder() }
-func (c testZenContext) ClientIP() string                    { return "" }
-func (c testZenContext) FullPath() string                    { return "" }
+// --- Context store ---
+func (c testZenContext) Set(string, any)                                    {}
+func (c testZenContext) Get(string) (any, bool)                             { return nil, false }
+func (c testZenContext) MustGet(string) any                                 { return nil }
+func (c testZenContext) Delete(string)                                      {}
+func (c testZenContext) GetString(string) string                            { return "" }
+func (c testZenContext) GetBool(string) bool                                { return false }
+func (c testZenContext) GetInt(string) int                                  { return 0 }
+func (c testZenContext) GetInt8(string) int8                                { return 0 }
+func (c testZenContext) GetInt16(string) int16                              { return 0 }
+func (c testZenContext) GetInt32(string) int32                              { return 0 }
+func (c testZenContext) GetInt64(string) int64                              { return 0 }
+func (c testZenContext) GetUint(string) uint                                { return 0 }
+func (c testZenContext) GetUint8(string) uint8                              { return 0 }
+func (c testZenContext) GetUint16(string) uint16                            { return 0 }
+func (c testZenContext) GetUint32(string) uint32                            { return 0 }
+func (c testZenContext) GetUint64(string) uint64                            { return 0 }
+func (c testZenContext) GetFloat32(string) float32                          { return 0 }
+func (c testZenContext) GetFloat64(string) float64                          { return 0 }
+func (c testZenContext) GetTime(string) time.Time                           { return time.Time{} }
+func (c testZenContext) GetDuration(string) time.Duration                   { return 0 }
+func (c testZenContext) GetStringSlice(string) []string                     { return nil }
+func (c testZenContext) GetStringMap(string) map[string]any                 { return nil }
+func (c testZenContext) GetStringMapString(string) map[string]string        { return nil }
+func (c testZenContext) GetStringMapStringSlice(string) map[string][]string { return nil }
 
-// --- Store ---
-func (c testZenContext) Set(string, any)                             {}
-func (c testZenContext) Get(string) (any, bool)                      { return nil, false }
-func (c testZenContext) MustGet(string) any                          { return nil }
-func (c testZenContext) GetString(string) string                     { return "" }
-func (c testZenContext) GetBool(string) bool                         { return false }
-func (c testZenContext) GetInt(string) int                           { return 0 }
-func (c testZenContext) GetInt64(string) int64                       { return 0 }
-func (c testZenContext) GetFloat64(string) float64                   { return 0 }
-func (c testZenContext) GetStringSlice(string) []string              { return nil }
-func (c testZenContext) GetStringMap(string) map[string]any          { return nil }
-func (c testZenContext) GetStringMapString(string) map[string]string { return nil }
-
-// --- Flow ---
-func (c testZenContext) Next()                        {}
-func (c testZenContext) Abort()                       {}
-func (c testZenContext) AbortWithStatus(int)          {}
-func (c testZenContext) AbortWithStatusJSON(int, any) {}
-func (c testZenContext) IsAborted() bool              { return false }
+// --- Error tracking & flow control ---
+func (c testZenContext) AddError(error)                   {}
+func (c testZenContext) Next()                            {}
+func (c testZenContext) Abort()                           {}
+func (c testZenContext) AbortWithStatus(int)              {}
+func (c testZenContext) AbortWithStatusJSON(int, any)     {}
+func (c testZenContext) AbortWithStatusPureJSON(int, any) {}
+func (c testZenContext) AbortWithError(int, error)        {}
+func (c testZenContext) IsAborted() bool                  { return false }
--- zen/context.go
+++ zen/context.go
@@ -1,100 +1,174 @@
 package zen
 
 import (
+	"context"
+	"io"
+	"io/fs"
 	"mime/multipart"
 	"net/http"
+	"time"
 )
 
 // Context abstracts the HTTP request/response context.
-// It wraps the underlying engine's context but exposes a framework-neutral API.
-// All gin.Context methods are available, ensuring full compatibility.
+// It embeds context.Context so it can be passed directly to context-aware functions.
+//
+// For gin-specific features that require gin types (Negotiate, Render,
+// ShouldBindWith, MustBindWith, etc.) use zgin.GinContext(c) to obtain the
+// underlying *gin.Context directly.
 type Context interface {
-	// --- Path / Query params ---
+	context.Context
+
+	// --- URL params ---
 	Param(key string) string
+	AddParam(key, value string)
+
+	// --- Query string ---
 	Query(key string) string
 	DefaultQuery(key, defaultValue string) string
 	GetQuery(key string) (string, bool)
 	QueryArray(key string) []string
+	GetQueryArray(key string) ([]string, bool)
 	QueryMap(key string) map[string]string
+	GetQueryMap(key string) (map[string]string, bool)
 
-	// --- Request headers ---
+	// --- Request info ---
 	Header(key string) string
 	SetHeader(key, value string)
 	ContentType() string
 	GetRawData() ([]byte, error)
 	IsWebsocket() bool
+	RemoteIP() string
+	ClientIP() string
+	FullPath() string
+	Request() *http.Request
+	ResponseWriter() http.ResponseWriter
+
+	// --- Handler introspection ---
+	HandlerName() string
+	HandlerNames() []string
 
 	// --- Bind (writes HTTP 400 on error) ---
 	Bind(v any) error
 	BindJSON(v any) error
+	BindXML(v any) error
 	BindQuery(v any) error
+	BindYAML(v any) error
+	BindTOML(v any) error
+	BindPlain(v any) error
 	BindUri(v any) error
 	BindHeader(v any) error
 
 	// --- ShouldBind (returns error, no auto response) ---
 	ShouldBind(v any) error
 	ShouldBindJSON(v any) error
+	ShouldBindXML(v any) error
 	ShouldBindQuery(v any) error
+	ShouldBindYAML(v any) error
+	ShouldBindTOML(v any) error
+	ShouldBindPlain(v any) error
 	ShouldBindUri(v any) error
 	ShouldBindHeader(v any) error
+
+	// ShouldBindBodyWith* re-reads the body from an internal cache — safe for
+	// multiple consecutive binds in the same request.
+	ShouldBindBodyWithJSON(v any) error
+	ShouldBindBodyWithXML(v any) error
+	ShouldBindBodyWithYAML(v any) error
+	ShouldBindBodyWithTOML(v any) error
+	ShouldBindBodyWithPlain(v any) error
 
 	// --- Form ---
 	PostForm(key string) string
 	DefaultPostForm(key, defaultValue string) string
 	GetPostForm(key string) (string, bool)
 	PostFormArray(key string) []string
+	GetPostFormArray(key string) ([]string, bool)
 	PostFormMap(key string) map[string]string
+	GetPostFormMap(key string) (map[string]string, bool)
 
-	// --- Multipart / File upload ---
+	// --- Multipart / file upload ---
 	FormFile(name string) (*multipart.FileHeader, error)
 	MultipartForm() (*multipart.Form, error)
-	SaveUploadedFile(file *multipart.FileHeader, dst string) error
+	SaveUploadedFile(file *multipart.FileHeader, dst string, perm ...fs.FileMode) error
 
 	// --- Cookie ---
 	Cookie(name string) (string, error)
 	SetCookie(name, value string, maxAge int, path, domain string, secure, httpOnly bool)
+	SetCookieData(cookie *http.Cookie)
 	SetSameSite(samesite http.SameSite)
 
 	// --- Response: JSON variants ---
-	JSON(code int, v any)
-	SecureJSON(code int, v any)
+	JSON(code int, obj any)
+	SecureJSON(code int, obj any)
 	JSONP(code int, obj any)
 	IndentedJSON(code int, obj any)
 	AsciiJSON(code int, obj any)
 	PureJSON(code int, obj any)
 
 	// --- Response: other formats ---
+	HTML(code int, name string, obj any)
 	XML(code int, obj any)
 	YAML(code int, obj any)
+	TOML(code int, obj any)
+	ProtoBuf(code int, obj any)
+	BSON(code int, obj any)
 	String(code int, format string, values ...any)
 	Data(code int, contentType string, data []byte)
+	DataFromReader(code int, contentLength int64, contentType string, reader io.Reader, extraHeaders map[string]string)
 	Status(code int)
 	Redirect(code int, location string)
 
-	// --- Underlying request / response ---
-	Request() *http.Request
-	ResponseWriter() http.ResponseWriter
-	ClientIP() string
-	FullPath() string
+	// --- File responses ---
+	File(filepath string)
+	FileFromFS(filepath string, fs http.FileSystem)
+	FileAttachment(filepath, filename string)
+
+	// --- Streaming ---
+	SSEvent(name string, message any)
+	Stream(step func(w io.Writer) bool) bool
+
+	// --- Content negotiation ---
+	NegotiateFormat(offered ...string) string
+	SetAccepted(formats ...string)
 
 	// --- Context store ---
 	Set(key string, value any)
 	Get(key string) (any, bool)
 	MustGet(key string) any
+	Delete(key string)
 	GetString(key string) string
 	GetBool(key string) bool
 	GetInt(key string) int
+	GetInt8(key string) int8
+	GetInt16(key string) int16
+	GetInt32(key string) int32
 	GetInt64(key string) int64
+	GetUint(key string) uint
+	GetUint8(key string) uint8
+	GetUint16(key string) uint16
+	GetUint32(key string) uint32
+	GetUint64(key string) uint64
+	GetFloat32(key string) float32
 	GetFloat64(key string) float64
+	GetTime(key string) time.Time
+	GetDuration(key string) time.Duration
 	GetStringSlice(key string) []string
 	GetStringMap(key string) map[string]any
 	GetStringMapString(key string) map[string]string
+	GetStringMapStringSlice(key string) map[string][]string
+
+	// --- Error tracking ---
+	// AddError appends err to the request's error list.
+	// Errors are available to downstream middleware (e.g., logging, recovery).
+	AddError(err error)
 
 	// --- Flow control ---
 	Next()
 	Abort()
 	AbortWithStatus(code int)
-	AbortWithStatusJSON(code int, v any)
+	AbortWithStatusJSON(code int, obj any)
+	AbortWithStatusPureJSON(code int, obj any)
+	AbortWithError(code int, err error)
 	IsAborted() bool
 }
 
